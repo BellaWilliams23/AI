d151b629dfe15e6b7b9a53e19f78f122f48e43cd
Williams <23678bella23678@gmail.com>
2024-03-14 14:02:51 -0400

    PrintingNamesIndex Stable- Pre ChatGPT

--- PrintingNamesIndex.py
+++ PrintingNamesIndex.py
@@ -2,3 +2,10 @@
 # Date: 3.14.2024
 # Resourse:https://youtu.be/jRAAaDll34Q?si=SZq8WSYzjrmuAoIA
 # Program: Printing Names Index
+
+names = ["Bianca", "Deanna", "Travis", "Chris", "Corey", "Brad"]
+
+index = 0
+for name in names:
+    print(index,name)
+    index += 1
